dfe23a2d9a0601438e8731584dc41d189feb0f24
Alexander Kraus <nr4@z10.info>
2018-07-03 16:09:00 +0200

    removed comments; added text feature.

--- tx210.py
+++ tx210.py
@@ -44,18 +44,24 @@ args, rest = parser.parse_known_args()
 
 text = rest[0]
 #print args.fontfile, args.size, args.italic, args.bold, "text = ", text
-fig = plt.figure()
+fig = plt.figure(figsize=(16,4))
 
 font = freetype.Face(args.fontfile)
 font.set_char_size(1000)
-for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
+xpos = 0
+for char in text :
     print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>",char
     font.load_char(char)
     glyph = font.glyph
     outline = glyph.outline
     points = np.array(outline.points, dtype=[('x',float), ('y',float)]) 
     x, y = points['x'], points['y']
-    print points
+    w = 300.
+    if len(x) > 0 :
+        w = abs(x.max() - x.min())*1.1
+    
+    for i in range(len(x)) :
+        x[i] += xpos
     
     for ic in range(len(outline.contours)) :
         c0 = 0
@@ -66,20 +72,16 @@ for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
             c1 = len(x)
         else : 
             c1 = outline.contours[ic]+1
-        #print "contour", c0, c1
         
         xa = x[c0:c1]
         ya = y[c0:c1]
         
         tx = outline.tags[c0:c1]
         
-        print "tags",tx
-        
         segments = []
         stack = []
         for i in range(len(tx)) :
             tag = tx[i]
-            #point = [ xa[i], ya[i] ]
             stack += [ tag ]
             
             if tag : 
@@ -87,24 +89,17 @@ for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
                 stack = [ tag ]
         stack += [ tx[0] ]
         segments += [ stack ]
-        print "segments=",segments
-        print "xa has len:", len(xa)
         xp = []
         yp = []
         
         j = 0
         for i in range(len(segments)) :
             segment = segments[i]
-            #print "drawing seg:",segment
-            
             data = []
-            print "seg ",i,"j=",j,"data",segment
             if len(segment) == 2 :
-                print "j=",j
                 p0 = [xa[j], ya[j]]
                 j = (j+1)%len(xa)
                 p1 = [xa[j], ya[j]]
-                print "line",p0,p1
                 xp += [ (1.-t)*p0[0]+t*p1[0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ (1.-t)*p0[1]+t*p1[1] for t in np.arange(0., 1., 1./100.) ]
             elif len(segment) == 3 :
@@ -113,11 +108,9 @@ for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
                 p1 = [xa[j], ya[j]]
                 j = (j+1)%len(xa)
                 p2 = [xa[j], ya[j]]
-                print "spline",p0,p1,p2, "j=", j
                 xp += [ pow(1.-t,2.)*p0[0]+2.*(1.-t)*t*p1[0]+t*t*p2[0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ pow(1.-t,2.)*p0[1]+2.*(1.-t)*t*p1[1]+t*t*p2[1] for t in np.arange(0., 1., 1./100.) ]
             elif len(segment) > 3:
-                print "dynamic segment of size",len(segment)
                 p = []
                 for k in range(len(segment)-1) :
                     p += [ [xa[j], ya[j]] ]
@@ -127,7 +120,6 @@ for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
                 pf = []
                 for k in range(1, len(segment)-1) :
                     pf += [ [ .5*(p[k][0]+p[k+1][0]), .5*(p[k][1]+p[k+1][1]) ] ]
-                print "pf",pf    
                 xp += [ pow(1.-t,2.)*p[0][0]+2.*(1.-t)*t*p[1][0]+t*t*pf[0][0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ pow(1.-t,2.)*p[0][1]+2.*(1.-t)*t*p[1][1]+t*t*pf[0][1] for t in np.arange(0., 1., 1./100.) ]
                 
@@ -136,73 +128,7 @@ for char in 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789' :
                     yp += [ pow(1.-t,2.)*pf[k-1][1]+2.*(1.-t)*t*p[k+1][1]+t*t*pf[k][1] for t in np.arange(0., 1., 1./100.) ]
                 xp += [ pow(1.-t,2.)*pf[-1][0]+2.*(1.-t)*t*p[-2][0]+t*t*p[-1][0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ pow(1.-t,2.)*pf[-1][1]+2.*(1.-t)*t*p[-2][1]+t*t*p[-1][1] for t in np.arange(0., 1., 1./100.) ]
-                
-            #elif len(segment) == 4 :
-                #p0 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p1 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p2 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p3 = [xa[j], ya[j]]
-
-                #pf = [ .5*(p1[k]+p2[k]) for k in range(2) ] 
-                
-                #print "wide spline",p0,p1,p2, "j=", j
-                #xp += [ pow(1.-t,2.)*p0[0]+2.*(1.-t)*t*p1[0]+t*t*pf[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*p0[1]+2.*(1.-t)*t*p1[1]+t*t*pf[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf[0]+2.*(1.-t)*t*p2[0]+t*t*p3[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf[1]+2.*(1.-t)*t*p2[1]+t*t*p3[1] for t in np.arange(0., 1., 1./100.) ]
-            #elif len(segment) == 5 :
-                #p0 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p1 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p2 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p3 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p4 = [xa[j], ya[j]]
-                
-                #pf1 = [ .5*(p1[k]+p2[k]) for k in range(2) ]
-                #pf2 = [ .5*(p2[k]+p3[k]) for k in range(2) ]
-                
-                #print "ultra wide spline",p0,p1,p2, "j=", j
-                #xp += [ pow(1.-t,2.)*p0[0]+2.*(1.-t)*t*p1[0]+t*t*pf1[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*p0[1]+2.*(1.-t)*t*p1[1]+t*t*pf1[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf1[0]+2.*(1.-t)*t*p2[0]+t*t*pf2[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf1[1]+2.*(1.-t)*t*p2[1]+t*t*pf2[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf2[0]+2.*(1.-t)*t*p3[0]+t*t*p4[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf2[1]+2.*(1.-t)*t*p3[1]+t*t*p4[1] for t in np.arange(0., 1., 1./100.) ]
-            #elif len(segment) == 6 :
-                #p0 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p1 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p2 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p3 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p4 = [xa[j], ya[j]]
-                #j = (j+1)%len(xa)
-                #p5 = [xa[j], ya[j]]
-                
-                #pf1 = [ .5*(p1[k]+p2[k]) for k in range(2) ]
-                #pf2 = [ .5*(p2[k]+p3[k]) for k in range(2) ]
-                #pf3 = [ .5*(p3[k]+p4[k]) for k in range(2) ]
-                
-                #print "ultra wide spline",p0,p1,p2, "j=", j
-                #xp += [ pow(1.-t,2.)*p0[0]+2.*(1.-t)*t*p1[0]+t*t*pf1[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*p0[1]+2.*(1.-t)*t*p1[1]+t*t*pf1[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf1[0]+2.*(1.-t)*t*p2[0]+t*t*pf2[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf1[1]+2.*(1.-t)*t*p2[1]+t*t*pf2[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf2[0]+2.*(1.-t)*t*p3[0]+t*t*pf3[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf2[1]+2.*(1.-t)*t*p3[1]+t*t*pf3[1] for t in np.arange(0., 1., 1./100.) ]
-                #xp += [ pow(1.-t,2.)*pf3[0]+2.*(1.-t)*t*p4[0]+t*t*p5[0] for t in np.arange(0., 1., 1./100.) ]
-                #yp += [ pow(1.-t,2.)*pf3[1]+2.*(1.-t)*t*p4[1]+t*t*p5[1] for t in np.arange(0., 1., 1./100.) ]
         plt.plot(xp, yp, 'o')
-    fig.savefig(char+'.png')
-    plt.clf()
+    xpos += w
+fig.savefig('text.png', width=2000.)
 
-#fig.show()
-#c = input()
