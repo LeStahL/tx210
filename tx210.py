b9dc6d4552f19cf4e07c422c6655ee982e086cfa
Alexander Kraus <nr4@z10.info>
2018-07-04 13:28:58 +0200

    implemented shader output

--- tx210.py
+++ tx210.py
@@ -38,25 +38,34 @@ plt.rc('axes', prop_cycle=(cycler.cycler('color', ['r', 'g', 'b', 'y'])))
 parser = argparse.ArgumentParser(description='Shader Text Generation Tool.')
 parser.add_argument('-f', '--fontfile', dest='fontfile')
 parser.add_argument('-s', '--size', dest='size')
-parser.add_argument('-i', '--italic', dest='italic', action='store_true')
-parser.add_argument('-b', '--bold', dest='bold', action='store_true')
+parser.add_argument('-o', '--output', dest='outfile')
 args, rest = parser.parse_known_args()
 
 text = rest[0]
-#print args.fontfile, args.size, args.italic, args.bold, "text = ", text
 fig = plt.figure(figsize=(16,4))
 
 font = freetype.Face(args.fontfile)
 font.set_char_size(1000)
 xpos = 0
+
+size = .05;
+if args.size != None :
+    size = float(args.size)
+
+otext = "float d=1.;\n"
 for char in text :
-    print ">>>>>>>>>>>>>>>>>>>>>>>>>>>>",char
+    otext += "//"+char+"\n"
     font.load_char(char)
     glyph = font.glyph
     outline = glyph.outline
     points = np.array(outline.points, dtype=[('x',float), ('y',float)]) 
     x, y = points['x'], points['y']
-    w = 300.
+    
+    for i in range(len(x)) :
+        x[i] = x[i]/1000.*size
+        y[i] = y[i]/1000.*size
+    
+    w = 300./1000.*size
     if len(x) > 0 :
         w = abs(x.max() - x.min())*1.1
     
@@ -102,15 +111,8 @@ for char in text :
                 p1 = [xa[j], ya[j]]
                 xp += [ (1.-t)*p0[0]+t*p1[0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ (1.-t)*p0[1]+t*p1[1] for t in np.arange(0., 1., 1./100.) ]
-            elif len(segment) == 3 :
-                p0 = [xa[j], ya[j]]
-                j = (j+1)%len(xa)
-                p1 = [xa[j], ya[j]]
-                j = (j+1)%len(xa)
-                p2 = [xa[j], ya[j]]
-                xp += [ pow(1.-t,2.)*p0[0]+2.*(1.-t)*t*p1[0]+t*t*p2[0] for t in np.arange(0., 1., 1./100.) ]
-                yp += [ pow(1.-t,2.)*p0[1]+2.*(1.-t)*t*p1[1]+t*t*p2[1] for t in np.arange(0., 1., 1./100.) ]
-            elif len(segment) > 3:
+                otext += "d = min(d, dsg(vec2("+"%.3f" %p0[0]+","+"%.3f" %p0[1]+"), vec2("+"%.3f" %p1[0]+","+"%.3f" %p1[1]+"), uv));\n"
+            elif len(segment) > 2:
                 p = []
                 for k in range(len(segment)-1) :
                     p += [ [xa[j], ya[j]] ]
@@ -120,15 +122,26 @@ for char in text :
                 pf = []
                 for k in range(1, len(segment)-1) :
                     pf += [ [ .5*(p[k][0]+p[k+1][0]), .5*(p[k][1]+p[k+1][1]) ] ]
+                
+                otext += "d = min(d, dsp(vec2("+"%.3f" %p[0][0]+","+"%.3f" %p[0][1]+"), vec2("+"%.3f" %p[1][0]+","+"%.3f" %p[1][1]+"), vec2("+"%.3f" %pf[0][0]+","+"%.3f" %pf[0][1]+"), uv));\n"
                 xp += [ pow(1.-t,2.)*p[0][0]+2.*(1.-t)*t*p[1][0]+t*t*pf[0][0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ pow(1.-t,2.)*p[0][1]+2.*(1.-t)*t*p[1][1]+t*t*pf[0][1] for t in np.arange(0., 1., 1./100.) ]
                 
                 for k in range(1, len(segment)-2) :
                     xp += [ pow(1.-t,2.)*pf[k-1][0]+2.*(1.-t)*t*p[k+1][0]+t*t*pf[k][0] for t in np.arange(0., 1., 1./100.) ]
                     yp += [ pow(1.-t,2.)*pf[k-1][1]+2.*(1.-t)*t*p[k+1][1]+t*t*pf[k][1] for t in np.arange(0., 1., 1./100.) ]
+                    otext += "d = min(d, dsp(vec2("+"%.3f" %pf[k-1][0]+","+"%.3f" %pf[k-1][1]+"), vec2("+"%.3f" %p[k+1][0]+","+"%.3f" %p[k+1][1]+"), vec2("+"%.3f" %pf[k][0]+","+"%.3f" %pf[k][1]+"), uv));\n"
                 xp += [ pow(1.-t,2.)*pf[-1][0]+2.*(1.-t)*t*p[-2][0]+t*t*p[-1][0] for t in np.arange(0., 1., 1./100.) ]
                 yp += [ pow(1.-t,2.)*pf[-1][1]+2.*(1.-t)*t*p[-2][1]+t*t*p[-1][1] for t in np.arange(0., 1., 1./100.) ]
+                otext += "d = min(d, dsp(vec2("+"%.3f" %pf[-1][0]+","+"%.3f" %pf[-1][1]+"), vec2("+"%.3f" %p[-2][0]+","+"%.3f" %p[-2][1]+"), vec2("+"%.3f" %p[-1][0]+","+"%.3f" %p[-1][1]+"), uv));\n"
         plt.plot(xp, yp, 'o')
     xpos += w
 fig.savefig('text.png', width=2000.)
 
+if args.outfile == None :
+    print otext
+else :
+    with open(args.outfile, "wt") as f:
+        f.write(otext)
+        f.close()
+    
